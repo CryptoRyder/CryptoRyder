83f886b6f0c996fd3b9d35c87985a40f0780cb4a
Matthew Black <mblackmblackmblack@gmail.com>
2018-02-03 22:27:48 -0500

    Fix deploy contracts

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,6 +1,7 @@
 var Ownable = artifacts.require("./zeppelin/ownership/Ownable.sol");
 var Killable = artifacts.require("./zeppelin/lifecycle/Killable.sol");
 var Authentication = artifacts.require("./Authentication.sol");
+var Rideshare = artifacts.require("./Rideshare.sol");
 
 module.exports = function(deployer) {
   deployer.deploy(Ownable);
@@ -8,4 +9,6 @@ module.exports = function(deployer) {
   deployer.deploy(Killable);
   deployer.link(Killable, Authentication);
   deployer.deploy(Authentication);
+  deployer.link(Killable, Rideshare);
+  deployer.deploy(Rideshare);
 };
